91efa49f1abf3fb1ce8f9a427d5c4b4e996c3ece
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-04-22 19:41:03 -0500

    Primeros cambios de producto

--- src/main/java/com/entrepiscoynazca/appweb/controller/ProductoController.java
+++ src/main/java/com/entrepiscoynazca/appweb/controller/ProductoController.java
@@ -1,5 +1,14 @@
 package com.entrepiscoynazca.appweb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.entrepiscoynazca.appweb.model.Producto;
+
+@Controller
 public class ProductoController{
 
     
