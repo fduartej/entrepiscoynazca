f0ff9330a1e35ca477b5965d1228e25712e62303
Fred <fduartej@gmail.com>
2021-07-01 21:03:30 -0500

    Fix file resource

--- src/main/java/com/entrepiscoynazca/appweb/controller/ReporteController.java
+++ src/main/java/com/entrepiscoynazca/appweb/controller/ReporteController.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ResourceUtils;
@@ -18,9 +20,11 @@ import net.sf.jasperreports.engine.*;
 public class ReporteController {
     
     private JdbcTemplate jdbcTemplate;
+    private ResourceLoader resourceLoader;
 
-    public ReporteController(JdbcTemplate jdbcTemplate){
+    public ReporteController(JdbcTemplate jdbcTemplate, ResourceLoader resourceLoader){
         this.jdbcTemplate = jdbcTemplate;
+        this.resourceLoader = resourceLoader;
     }
 
     @GetMapping("/jasper/repventas")
@@ -29,8 +33,8 @@ public class ReporteController {
         response.setHeader("Content-Disposition", String.format("attachment; filename=\"ventas.pdf\""));
         try {
             OutputStream out = response.getOutputStream();
-            File file = ResourceUtils.getFile("classpath:./reports/ReporteDeVentas.jrxml");
-            JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+            Resource resource = resourceLoader.getResource("classpath:./reports/ReporteDeVentas.jrxml");
+            JasperReport jasperReport = JasperCompileManager.compileReport(resource.getInputStream());
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, 
                     jdbcTemplate.getDataSource().getConnection());
             JasperExportManager.exportReportToPdfStream(jasperPrint, out);
